f9a23e7c7123bcca1ee97c0aedbb8974b6626346
Shavinda Dissanayake <shavinda.diz@gmail.com>
2024-10-07 17:15:44 +0530

    Initial Stage Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    extra.apply {
+        set("compose_compiler_version", "1.5.3")
+        set("lifecycle_version", "2.6.2")
+    }
+}
+
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.android.application") version "8.5.2" apply false
+    id("com.android.library") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
